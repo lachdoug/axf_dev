c0652d88b877d65c1964a02d9f6c52acc5f15fca
Lachlan Douglas <lachlan@engines.org>
2018-06-12 11:45:32 +1000

    cors

--- v0/api/controllers/power/meters_controller.rb
+++ v0/api/controllers/power/meters_controller.rb
@@ -2,12 +2,14 @@ class V0
   module Api
     module Controllers
 
-      
+
       get '/power/meters' do
+        response['Access-Control-Allow-Origin'] = '*'
         test_meters
       end
 
       get '/power/meters/:id' do
+        response['Access-Control-Allow-Origin'] = '*'
         id = params[:id].to_i
         return 404 if id < 1 || id > test_meters.length
         result = test_meters[ id - 1 ]
